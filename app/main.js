f7c0f4b224fec5270add3901ebbd45fd385fdc08
tomeidk <cecepas2001@gmail.com>
2024-07-14 23:31:25 +0200

    pass stage 6

--- app/main.js
+++ app/main.js
@@ -1,25 +1,38 @@
+const fs = require("fs");
 const net = require("net");
 
 const server = net.createServer((socket) => {
-
-    // Request
+  // Request
   socket.on("data", (data) => {
     const request = data.toString();
-    const url = request.split(' ')[1];
+    const url = request.split(" ")[1];
     const headers = request.split("\r\n");
 
     if (url == "/") {
-        socket.write("HTTP/1.1 200 OK\r\n\r\n");
+      socket.write("HTTP/1.1 200 OK\r\n\r\n");
     } else if (url.includes("/echo")) {
-        const content = url.split('/echo/')[1];
+      const content = url.split("/echo/")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`
+      );
+    } else if (url.startsWith("/files/")) {
+      const directory = process.argv[3];
+      const filename = url.split("/files")[1];
+      if (fs.existsSync(`${directory}/${filename}`)) {
+        const content = fs.readFileSync(`${directory}/${filename}`).toString();
         socket.write(
-          `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`
+          `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`
         );
+      } else {
+        socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+      }
     } else if (url == "/user-agent") {
-        const userAgent = headers[2].split('User-Agent: ')[1];
-        socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`);
+      const userAgent = headers[2].split("User-Agent: ")[1];
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
+      );
     } else {
-        socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
     socket.end();
