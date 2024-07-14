d23d0522c310ba6d8334df5b866f80f3fc97d32c
tomeidk <cecepas2001@gmail.com>
2024-07-14 23:02:12 +0200

    pass stage 2

--- app/main.js
+++ app/main.js
@@ -1,7 +1,19 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-  socket.write("HTTP/1.1 200 OK\r\n\r\n");
+//   socket.write("HTTP/1.1 200 OK\r\n\r\n");
+
+  socket.on("data", (data) => {
+    const request = data.toString();
+    if(request.startsWith('GET / ')) {
+        const httpResponse = 'HTTP/1.1 200 OK\r\n\r\n';
+        socket.write(httpResponse);
+    } else {
+        const httpResponse = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        socket.write(httpResponse);
+    }
+    socket.end();
+  });
 
   socket.on("close", () => {
     socket.end();
