e4ebdd29db5287029719fd8a2e9206b2e2acfd71
tomeidk <cecepas2001@gmail.com>
2024-07-15 13:49:57 +0200

    pass stage 8

--- app/main.js
+++ app/main.js
@@ -19,7 +19,7 @@ const server = net.createServer((socket) => {
         const encodings = headers[2].split("Accept-Encoding: ")[1].split(",");
         console.log("encs: " + encodings);
         console.log("encs check:" + encodings[1]);
-        if (encodings.includes("gzip")) {
+        if (encodings.includes(" gzip")) {
           socket.write(
             `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\nContent-Encoding: gzip\r\n\r\n${content}`
           );
