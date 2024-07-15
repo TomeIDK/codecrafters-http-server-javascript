56469482b6d2638c8a8f4abcb91449b5e83ed01f
tomeidk <cecepas2001@gmail.com>
2024-07-15 13:11:27 +0200

    pass stage 7

--- app/main.js
+++ app/main.js
@@ -6,17 +6,19 @@ const server = net.createServer((socket) => {
   socket.on("data", (data) => {
     const request = data.toString();
     const url = request.split(" ")[1];
-    const method = request.split(" ")[0];
     const headers = request.split("\r\n");
 
-    if (url == "/") { // GET
+    if (url == "/") {
+      // GET
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (url.startsWith("/echo")) { // GET echo endpoint
+    } else if (url.includes("/echo")) {
+      // GET echo endpoint
       const content = url.split("/echo/")[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`
       );
-    } else if (url.startsWith("/files/") && method === "GET") { // GET file endpoint
+    } else if (url.startsWith("/files/")) {
+      // GET file endpoint
       const directory = process.argv[3];
       const filename = url.split("/files")[1];
       if (fs.existsSync(`${directory}/${filename}`)) {
@@ -27,19 +29,23 @@ const server = net.createServer((socket) => {
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
-    } else if (url == "/user-agent") { // GET user agent endpoint
+    } else if (url == "/user-agent") {
+      // GET user agent endpoint
       const userAgent = headers[2].split("User-Agent: ")[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
-    } else if (url.startsWith("/files/") && method === "POST") {
-      const filename = process.argv[3] + "/" + url.substring(7);
-
-      const req = data.toString().split("\r\n");
-      const body = req[req.length -1];
-      fs.writeFileSync(filename, body);
-      socket.write(`HTTP/1.1 201 CREATED\r\n\r\n`);
-    } else { // GET NOT FOUND
+    } else if (
+      url.startsWith("/files") &&
+      data.toString().split(" "[0] === "POST")
+    ) {
+      let fileName = url.split("/")[2];
+      const filePath = FILES_DIR + fileName;
+      const file = data.toString("utf-8").split("\r\n\r\n")[1];
+      fs.writeFileSync(filePath, file);
+      socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
+    } else {
+      // GET NOT FOUND
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
