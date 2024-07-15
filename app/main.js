e599fd43a3c397dea7ddd04c689273233c801e7d
tomeidk <cecepas2001@gmail.com>
2024-07-15 13:03:35 +0200

    pass stage 7

--- app/main.js
+++ app/main.js
@@ -5,17 +5,17 @@ const server = net.createServer((socket) => {
   // Request
   socket.on("data", (data) => {
     const request = data.toString();
-    const url = request.split(" ")[1];
+    const [method, url, protocol] = request[0].split(" ");
     const headers = request.split("\r\n");
 
-    if (url == "/") {
+    if (url == "/") { // GET
       socket.write("HTTP/1.1 200 OK\r\n\r\n");
-    } else if (url.includes("/echo")) {
+    } else if (url.includes("/echo")) { // GET echo endpoint
       const content = url.split("/echo/")[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n\r\n`
       );
-    } else if (url.startsWith("/files/")) {
+    } else if (url.startsWith("/files/") && method === "GET") { // GET file endpoint
       const directory = process.argv[3];
       const filename = url.split("/files")[1];
       if (fs.existsSync(`${directory}/${filename}`)) {
@@ -26,12 +26,19 @@ const server = net.createServer((socket) => {
       } else {
         socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
       }
-    } else if (url == "/user-agent") {
+    } else if (url == "/user-agent") { // GET user agent endpoint
       const userAgent = headers[2].split("User-Agent: ")[1];
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`
       );
-    } else {
+    } else if (url.startsWith("/files/") && method === "POST"){
+      const filename = process.argv[3] + "/" + url.substring(7);
+
+      const req = data.toString().split("\r\n");
+      const body = req[req.length -1];
+      fs.writeFileSync(filename, body);
+      socket.write(`HTTP/1.1 201 CREATED\r\n\r\n`);
+    } else { // GET NOT FOUND
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
 
