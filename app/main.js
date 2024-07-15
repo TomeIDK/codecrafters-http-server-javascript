f08236b2c504ebc9b882d82bb77e1aa1ce225c7c
tomeidk <cecepas2001@gmail.com>
2024-07-15 13:21:27 +0200

    pass stage 7

--- app/main.js
+++ app/main.js
@@ -40,7 +40,7 @@ const server = net.createServer((socket) => {
 
       const body = request[request.length - 1]
       fs.writeFileSync(fileName, body);
-      socket.write("HTTP/1.1 201 CREATED\r\n\r\n");
+      socket.write("HTTP/1.1 201 Created\r\n\r\n");
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
     }
